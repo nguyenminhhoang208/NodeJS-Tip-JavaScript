4178df6079b3810d6f6def32705fdb54adf1e29e
Minh Hoang <nmhoangvjppro@gmail.com>
2023-05-06 08:13:04 +0700

    set up some package important

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,21 @@
-import express, { Express } from 'express';
+import compression from 'compression';
+import express, { Express, NextFunction, Request, Response } from 'express';
+const morgan = require('morgan');
+const { default: helmet } = require('helmet');
 const app: Express = express();
+
+// init middlewares
+
+app.use(morgan('dev')); // log request
+
+app.use(helmet()); // bảo vệ những thông tin riêng tư
+
+app.use(compression()); // giảm kích thước dữ liệu vận chuyển
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+	return res.status(200).json({
+		message: 'Hello World!!',
+	});
+});
 
 module.exports = app;
